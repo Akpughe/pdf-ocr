3022d29b4d16c4a7cd959091325607db0c1a965c
“Akpughe” <davidakpughe2@gmail.com>
2024-11-11 23:34:52 +0100

    add bull-mq bull-board

--- app.ts
+++ app.ts
@@ -3,10 +3,44 @@ import multer from "multer";
 import { extractTextFromPDF } from "./src/ocrService";
 import util from "util";
 import cors from "cors";
+import { createBullBoard } from "@bull-board/api";
+import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
+import { ExpressAdapter } from "@bull-board/express";
+import { Queue } from "bullmq";
+import IORedis from "ioredis";
+import dotenv from "dotenv";
+import {
+  formatYouTubeLinkAndGetID,
+  getTranscriptFromVideo,
+} from "./src/ytExtraction";
 
 const app = express();
 
 const port = process.env.PORT || 4000;
+
+const redis_url = process.env.REDIS_URL || "redis://localhost:6379";
+
+const router = express.Router();
+
+// Redis connection
+export const connection = new IORedis(redis_url, {
+  maxRetriesPerRequest: null,
+  enableReadyCheck: false,
+});
+
+export const subscriptionQueue = new Queue("subscription-queue", {
+  connection,
+});
+
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath("/admin/queues");
+
+const { addQueue, removeQueue, setQueues, replaceQueues } = createBullBoard({
+  queues: [new BullMQAdapter(subscriptionQueue)],
+  serverAdapter,
+});
+
+app.use("/admin/queues", serverAdapter.getRouter());
 
 // allow body parser
 app.use(express.json());
@@ -59,6 +93,24 @@ app.post(
   }
 );
 
+app.post("/yt-ocr", async (req: Request, res: Response) => {
+  const { url } = req.body;
+
+  try {
+    const { formattedUrl } = formatYouTubeLinkAndGetID(url);
+
+    const transcript = await getTranscriptFromVideo(formattedUrl);
+
+    res
+      .status(200)
+      .json({ title: transcript?.title, text: transcript?.transcriptText });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to get transcript from YouTube video" });
+  }
+});
+
 // catch errors
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
@@ -67,4 +119,5 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
+  console.log(`BullMQ UI: http://localhost:${port}/admin/queues`);
 });
